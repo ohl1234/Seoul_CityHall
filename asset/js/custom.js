32ded1933315cf69882146c5d5b8f8886bf8f6a3
ohhyerim <tmvkfmxk00@naver.com>
2022-07-23 02:59:59 +0900

    하루마무리

---
+++ asset/js/custom.js
@@ -0,0 +1,18 @@
+$(function(){
+    var swiper = new Swiper(".main-slide", {
+        spaceBetween: 0,
+        centeredSlides: true,
+        autoplay: {
+          delay: 5000,
+          disableOnInteraction: false,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+})
