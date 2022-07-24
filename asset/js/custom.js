3734819a8df7f61b462a5a06f146a2fdfd729c8d
ohhyerim <tmvkfmxk00@naver.com>
2022-07-24 19:45:31 +0900

    html css 마무리

--- asset/js/custom.js
+++ asset/js/custom.js
@@ -1,5 +1,5 @@
 $(function(){
-  // swiper slide
+  // main swiper slide
     var swiper01 = new Swiper(".main-slide", {
         spaceBetween: 0,
         centeredSlides: true,
@@ -8,7 +8,7 @@ $(function(){
           disableOnInteraction: false,
         },
         pagination: {
-          el: ".pager",
+          el: ".pager1",
           type: "fraction",
         },
         navigation: {
@@ -16,6 +16,7 @@ $(function(){
           prevEl: ".prev",
         },
       });
+
     //autoplay
     $('.pause').on('click',function(event){
       $(this).removeClass('active').siblings('.play').addClass('active');
@@ -36,4 +37,41 @@ $(function(){
 
       return false;
     });
+
+
+  //sub swiper slider
+  var swiper02 = new Swiper(".sub-slide", {
+    slidesPerView: 3,
+    spaceBetween: 43,
+    loop:true,
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".pager2",
+      type: "fraction",
+    },
+    navigation: {
+      nextEl: ".next",
+      prevEl: ".prev",
+    }
+  }); 
+
+
+  //sc-site
+  $('.sub-list').css('display','none'); // 전부 display none 처리
+  $('.site-item > a').click(function(event){
+    $('.sub-list').stop().slideUp(); // 모든 sub-list닫아두고
+    $(this).siblings('.sub-list').stop().slideToggle(200); // 해당하는 sub-list만 토글처리
+
+    $('.ic-arrow').removeClass('active');
+    $(this).find('.ic-arrow').toggleClass('active');
+
+    $('.site-item > a').removeClass('active');
+    $(this).toggleClass('active');
+
+    // 해당 a 다시 눌렀을때 전부 removeClass
+    return false;
+  });
 })
